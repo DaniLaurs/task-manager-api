65dd407c53b4d3ae2b0ddd2a3bf99be1f575c6c4
DaniLaurs <sousal22@outlook.com>
2025-11-04 15:53:34 -0300

    fix : ajusta tsconfig para build no render

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,22 +1,26 @@
+import dotenv from 'dotenv';
 import z from 'zod';
 
+// 🔹 Carrega as variáveis do arquivo .env antes de qualquer validação
+dotenv.config();
+
 const envSchema = z.object({
-    PORT: z
-        .string()
-        .min(4, { message: 'Número da porta dever ter pelo menos 4 caracteres.' })
-        .transform(value => Number(value)),
-    HOST_USERNAME: z
-        .string()
-        .min(1, { message: 'O valor do HOST_USERNAME é obrigatório.' }),
-    HOST_PASSWORD: z
-        .string()
-        .min(1, { message: 'O valor do HOST_PASSOWORD é obrigatório.' }),
-    HOST_CLUSTER: z
-        .string()
-        .min(1, { message: 'O valor do HOST_CLUSTER é obrigatório.' }),
-    HOST_DATABASE: z
-        .string()
-        .min(1, { message: 'O valor do HOST_DATABASE é obrigatório.' }),
+  PORT: z
+    .string()
+    .min(4, { message: 'Número da porta deve ter pelo menos 4 caracteres.' })
+    .transform(value => Number(value)),
+  HOST_USERNAME: z
+    .string()
+    .min(1, { message: 'O valor do HOST_USERNAME é obrigatório.' }),
+  HOST_PASSWORD: z
+    .string()
+    .min(1, { message: 'O valor do HOST_PASSWORD é obrigatório.' }),
+  HOST_CLUSTER: z
+    .string()
+    .min(1, { message: 'O valor do HOST_CLUSTER é obrigatório.' }),
+  HOST_DATABASE: z
+    .string()
+    .min(1, { message: 'O valor do HOST_DATABASE é obrigatório.' }),
 });
 
 export const env = envSchema.parse(process.env);
